b47926589b884451fdfda382fd591682610de2fb
Addy-codes <adeeb9839@gmail.com>
2024-01-03 19:32:15 +0530

    urls fixed

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('', include('users.urls')),
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
                                            authentication_form=LoginForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/forms.py
+++ users/forms.py
@@ -62,7 +62,6 @@ class UpdateUserForm(forms.ModelForm):
 class UpdateProfileForm(forms.ModelForm):
     profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
     bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-
     class Meta:
         model = Profile
         fields = ['profile_picture', 'bio']
